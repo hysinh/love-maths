b5c3d65083e380e82f982aff91f1dc5eef30d585
hysinh <156194500+hysinh@users.noreply.github.com>
2024-03-28 09:58:51 +0000

    Adds code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -73,11 +75,23 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the currect score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
+     
 }
 
+/**
+ * Gets the currect incorrect score from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldIncorrect = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldIncorrect;
 
 }
 
